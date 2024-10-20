66e126e5dd431180172014d1142e4b5bb6826833
Pramuditha <pramudithakavinda01@gmail.com>
2024-10-20 12:52:45 +0530

    Create Blog Cart Image

--- the-explorer/app/page.js
+++ the-explorer/app/page.js
@@ -1,9 +1,11 @@
+import Blogitem from "@/Components/Blogitem";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Blogitem/>
     </>
   );
 }
