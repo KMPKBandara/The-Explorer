7a492eb1f3a2721e4d9234f6e10343e3815894de
Pramuditha <pramudithakavinda01@gmail.com>
2025-02-16 23:49:15 +0530

    Update sidebar in admin pannel

--- the-explorer/Components/AdminComponents/Sidebar.jsx
+++ the-explorer/Components/AdminComponents/Sidebar.jsx
@@ -8,6 +8,14 @@ const Sidebar = () => {
         <div className='px-2 sm:pl-14 py-3 border border-black'>
             <Image src={assets.logo} width={120} alt=''/>
         </div>
+        <div className='w-28 sm:w-80 h-[100vh] relative py-12 border border-black'>
+          <div className='flex item-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
+            <div className='w-[50%] sm:w-[80%] absolute right-0'>
+              
+            </div>
+            <Image src={assets.add_icon} alt='' width={28}/><p>Add blogs</p>
+          </div>
+        </div>
       
     </div>
   )
