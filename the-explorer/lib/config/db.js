f91c61997cb411fb3ff99815806080babbeb424e
Pramuditha <pramudithakavinda01@gmail.com>
2025-02-25 19:47:14 +0530

    Ensure the Mongodb connected

--- the-explorer/lib/config/db.js
+++ the-explorer/lib/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-export const ConnectDB = async () =>{
-    await mongoose.connect('mongodb+srv://kmpkbandara:5jC7xuXwJX6xvW5z@cluster0.ivjmj.mongodb.net/the-explorer')
-    console.log("DB Connected");
-}
+export const ConnectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://kmpkbandara:5jC7xuXwJX6xvW5z@cluster0.ivjmj.mongodb.net/the-explorer"
+  );
+  console.log("DB Connected");
+};
